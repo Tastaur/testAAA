2e4f81cde6fdcb2e71c9ea1a3101718cf875e57d
Mark <Trrrrex93@gmail.com>
2020-06-15 23:41:10 +0400

    Updates

---
+++ precache-manifest.ae56e022882822f163421a06f744796a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be12b80a74bc18f8e42e8e494b06e00b",
+    "url": "/testAAA/index.html"
+  },
+  {
+    "revision": "6d3bbf42c70e567d5e90",
+    "url": "/testAAA/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b5847c65a80cd0332b34",
+    "url": "/testAAA/static/css/main.e7ef496f.chunk.css"
+  },
+  {
+    "revision": "6d3bbf42c70e567d5e90",
+    "url": "/testAAA/static/js/2.ae8cf040.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/testAAA/static/js/2.ae8cf040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5847c65a80cd0332b34",
+    "url": "/testAAA/static/js/main.4021d8aa.chunk.js"
+  },
+  {
+    "revision": "0674ac289a1babf3c99d",
+    "url": "/testAAA/static/js/runtime-main.980224d2.js"
+  },
+  {
+    "revision": "f25fd40461c2a64bd191479ca199f9a2",
+    "url": "/testAAA/static/media/BebasNeue Regular.f25fd404.ttf"
+  }
+]);
